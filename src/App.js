cbeb1f8cb6df479f9d1ec165b4f4a2adbd56fe8d
sudheerkumar1996 <sudheerkvy1996@gmail.com>
2020-11-07 18:40:02 -1200

    higher order components

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
     <div className='App'>
       <UserList dataSource='https://jsonplaceholder.typicode.com/users' />
       <UserProfile
-        name='Yihua'
-        email='yihuazhang@gmail.com'
+        name='Sudheer kumar'
+        email='poojarysudhi1996@gmail.com'
         dataSource='https://jsonplaceholder.typicode.com/posts'
       />
     </div>
